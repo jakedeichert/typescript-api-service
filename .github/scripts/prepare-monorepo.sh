c2188698beed3d8ce69af2f28431a336de6903b3
Jake Deichert <git@jakedeichert.com>
2020-01-26 16:01:01 -0500

    Really fix mask binary ref

--- .github/scripts/prepare-monorepo.sh
+++ .github/scripts/prepare-monorepo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install mask
-curl -OL https://github.com/jakedeichert/mask/releases/download/v0.7.1/mask-v0.7.1-x86_64-apple-darwin.zip \
+curl -OL https://github.com/jakedeichert/mask/releases/download/v0.7.1/mask-v0.7.1-x86_64-unknown-linux-gnu.zip \
     && unzip mask*.zip \
     && mv mask-*/mask /usr/share/rust/.cargo/bin
 
