2eddac047cdefcc817120f7a3aed7691d9f0874f
Jake Deichert <jacob.deichert@staydomio.com>
2020-02-15 17:21:35 -0500

    Catch invalid json request body error

--- packages/http-utils/src/middleware/body-parser.ts
+++ packages/http-utils/src/middleware/body-parser.ts
@@ -1,8 +1,13 @@
 import koaBodyParser from 'koa-bodyparser';
 import { MiddlewareHandler } from '../middleware';
+import { boom } from '../index';
 
 export default (requestBodyMbLimit: number): MiddlewareHandler => {
     return koaBodyParser({
         jsonLimit: `${requestBodyMbLimit}mb`,
+        strict: true,
+        onerror: err => {
+            throw boom.badRequest(`Failed to parse json body: ${err.message}`);
+        },
     });
 };
